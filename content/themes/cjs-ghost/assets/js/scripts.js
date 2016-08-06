c6a8f8ce0d5928cdd1a5944b654dc3f41bc49f0f
Chad Sheets <chad@cjsheets.com>
2016-08-06 14:33:55 -0800

    Updated site to include images on post stubs, refactored javascript

--- content/themes/cjs-ghost/assets/js/scripts.js
+++ content/themes/cjs-ghost/assets/js/scripts.js
@@ -3,19 +3,7 @@ jQuery(function($) {
 $( window ).load(function() {
   prettyPrint();
   updateSocialLinks();
-});
-
-function updateSocialLinks() {
-  $('#facebook-share-button, #facebook-share-button-tail').attr("href", "https://facebook.com/sharer/sharer.php?u=http%3A%2F%2Fchadsheets.com" + window.location.pathname.replace(/\//g,"%2F"));
-  $('#google-share-button, #google-share-button-tail').attr("href", "https://plus.google.com/share?url=http%3A%2F%2Fchadsheets.com" + window.location.pathname.replace(/\//g,"%2F"));
-  $('#twitter-share-button, #twitter-share-button-tail').attr("href", "https://twitter.com/intent/tweet/?text=" + document.title.replace(/ /g,"%20") + ";&url=http%3A%2F%2Fchadsheets.com" + window.location.pathname.replace(/\//g,"%2F"));
-  $('#linkedin-share-button, #linkedin-share-button-tail').attr("href", "https://www.linkedin.com/shareArticle?mini=true&url=http%3A%2F%2Fchadsheets.com" + window.location.pathname.replace(/\//g,"%2F") + "&title=" + document.title.replace(/ /g,"%20") + "&summary=" + document.title.replace(/ /g,"%20") + "&source=http%3A%2F%2Fchadsheets.com");
-}
-
-$('.post-stub').mouseover(function() {
-  $(this).css('border-right', '2px solid #417a97')
-}); $('.post-stub').mouseout(function() {
-  $(this).css('border-right', '2px solid #FFFFFF')
+  initPostStubs();
 });
 
 $('#social-sidebar').mouseover(function() {
@@ -49,11 +37,23 @@ $('svg').find('.svg-button').stop()
 //   $('.share-button-in').delay( 150 ).hide();
 // });
 
-$('#search-field').val('');
 
-    /* ============================================================ */
-    /* Hide Header on scroll */
-    /* ============================================================ */
+$(".post-content").fitVids();
+
+/** ============================
+ *  `To The Top` Link
+ */
+
+$('.js-jump-top').on('click', function(e) {
+    e.preventDefault();
+
+    $('html, body').animate({'scrollTop': 0});
+});
+
+
+/** ============================
+ *  Hide Page Header on Scroll
+ */
 
 var didScroll;
 var lastScrollTop = 0;
@@ -97,25 +97,26 @@ function hasScrolled() {
     lastScrollTop = st;
 }
 
-    /* ============================================================ */
-    /* Search Functions */
-    /* ============================================================ */
+
+/** ============================
+ *  Site Search
+ */
 
 var searchField = $("#search-field").ghostHunter({
-results     : ".container-search",
-rss         : "rss.xml",
-//Enable the "search as you type" by uncommenting the following line
-onKeyUp   : true,
-
-result_template     : "<div class='post-stub'> <a class='js-ajax-link' title='{{title}}' href='{{link}}'>" +
-    "<div class='post-stub-date-container'> <div><time class='post-stub-mm-yyyy' datetime='{{pubDate}}'>{{pubDate}}</time></div> </div> " +
-    "<div class='post-stub-content'> <h4>{{title}}</h4> <p>{{description}}</p> </div> " +
-    "<div style='width: 3px; background-color: 0000FF; float: right'></div> <div style='clear:both;'></div> </a> </div> <hr class='post-loop'>",
-info_template       : "<div style='width: auto; height: 50px; overflow: auto;'>&nbsp;</div><div class='search-header'><h1 class='post-title'>Site Search</h1><p>Found: {{amount}} post(s), searching for <b><span id='searching-for'></span></b></p></div><hr class='post-loop'>",
-tail_template   : "<p class='search-return-link'><a href='#' id='return-to-page'>Return to Previous Page</a></p>",
-
+    results     : ".container-search",
+    rss         : "rss.xml",
+    //Enable the "search as you type" by uncommenting the following line
+    onKeyUp   : true,
+    result_template     : "<div class='post-stub'> <a class='js-ajax-link' title='{{title}}' href='{{link}}'>" +
+        "<div class='post-stub-date-container'> <div><time class='post-stub-mm-yyyy' datetime='{{pubDate}}'>{{pubDate}}</time></div> </div> " +
+        "<div class='post-stub-content'> <h4>{{title}}</h4> <p>{{description}}</p> </div> " +
+        "<div style='width: 3px; background-color: 0000FF; float: right'></div> <div style='clear:both;'></div> </a> </div> <hr class='post-loop'>",
+    info_template       : "<div style='width: auto; height: 50px; overflow: auto;'>&nbsp;</div><div class='search-header'><h1 class='post-title'>Site Search</h1><p>Found: {{amount}} post(s), searching for <b><span id='searching-for'></span></b></p></div><hr class='post-loop'>",
+    tail_template   : "<p class='search-return-link'><a href='#' id='return-to-page'>Return to Previous Page</a></p>",
 });
 
+
+$('#search-field').val('');
 
 $('.container-search').hide();
 var searchResultContainer = "hidden";
@@ -144,189 +145,207 @@ $('#search-field').focusout(function() {
 });
 
 function clearResults() {
-searchField.clear();
+    searchField.clear();
 }
 
 $('#searchsubmit').click(function() {
     $('#search-field').keyup();
 });
 
-    /* ============================================================ */
-    /* Responsive Videos */
-    /* ============================================================ */
 
-    $(".post-content").fitVids();
 
-    /* ============================================================ */
-    /* Scroll To Top */
-    /* ============================================================ */
+/** ============================
+ *  AJAX Navigation
+ */
 
-    $('.js-jump-top').on('click', function(e) {
-        e.preventDefault();
+var History = window.History;
+var loading = false;
+var showIndex = false;
+var $ajaxContainer = $('#container-ajax');
+var $latestPost = $('.latest-post');
+var $postIndex = $('.post-index');
+
+// Initially hide the index and show the latest post
+$latestPost.show();
+$postIndex.hide();
+
+// Show the index if the url has "page" in it (a simple
+// way of checking if we're on a paginated page.)
+if (window.location.pathname.indexOf('page') === 1 || window.location.pathname.indexOf('tag') === 1) {
+    $latestPost.hide();
+    $postIndex.show();
+}
+
+// Check if history is enabled for the browser
+if ( ! History.enabled) {
+    return false;
+}
+
+History.Adapter.bind(window, 'statechange', function() {
+    var State = History.getState();
+
+    // Get the requested url and replace the current content
+    // with the loaded content
+    $.get(State.url, function(result) {
+        var $html = $(result);
+        var $newContent = $('#container-ajax', $html).contents();
+
+        // Set the title to the requested urls document title
+        document.title = $html.filter('title').text();
 
         $('html, body').animate({'scrollTop': 0});
-    });
 
-    /* ============================================================ */
-    /* Ajax Loading */
-    /* ============================================================ */
+        $ajaxContainer.fadeOut(500, function() {
+            $latestPost = $newContent.filter('.latest-post');
+            $postIndex = $newContent.filter('.post-index');
 
-    var History = window.History;
-    var loading = false;
-    var showIndex = false;
-    var $ajaxContainer = $('#container-ajax');
-    var $latestPost = $('.latest-post');
-    var $postIndex = $('.post-index');
-
-    // Initially hide the index and show the latest post
-    $latestPost.show();
-    $postIndex.hide();
-
-    // Show the index if the url has "page" in it (a simple
-    // way of checking if we're on a paginated page.)
-    if (window.location.pathname.indexOf('page') === 1 || window.location.pathname.indexOf('tag') === 1) {
-        $latestPost.hide();
-        $postIndex.show();
-    }
-
-    // Check if history is enabled for the browser
-    if ( ! History.enabled) {
-        return false;
-    }
-
-    History.Adapter.bind(window, 'statechange', function() {
-        var State = History.getState();
-
-        // Get the requested url and replace the current content
-        // with the loaded content
-        $.get(State.url, function(result) {
-            var $html = $(result);
-            var $newContent = $('#container-ajax', $html).contents();
-
-            // Set the title to the requested urls document title
-            document.title = $html.filter('title').text();
-
-            $('html, body').animate({'scrollTop': 0});
-
-            $ajaxContainer.fadeOut(500, function() {
-                $latestPost = $newContent.filter('.latest-post');
-                $postIndex = $newContent.filter('.post-index');
-
-                if (showIndex === true) {
-                    $latestPost.hide();
-                } else {
-                    $latestPost.show();
-                    $postIndex.hide();
-                }
-
-                // Re run fitvid.js
-                $newContent.fitVids();
-
-                $ajaxContainer.html($newContent);
-                $ajaxContainer.fadeIn(500);
-
-                NProgress.done();
-
-                loading = false;
-                showIndex = false;
-                /* Add on's */
-                prettyPrint();
-                updateSocialLinks();
-                $('.post-stub').mouseover(function() {
-                  $(this).css('border-right', '2px solid #417a97')
-                }); $('.post-stub').mouseout(function() {
-                  $(this).css('border-right', '2px solid #FFFFFF')
-                });
-                $latestPost = $('.latest-post');
-                $postIndex = $('.post-index');
-            });
-        }).fail(function() {
-            // Request fail
-            NProgress.done();
-            location.reload();
-        });
-    });
-
-    $('body').on('click', '.js-ajax-link, .pagination a, .post-tags a, .post-header a', function(e) {
-        e.preventDefault();
-        $('#search-field').val('');
-
-        if (loading === false) {
-            var currentState = History.getState();
-            var url = $(this).attr('href');
-            var title = $(this).attr('title') || null;
-
-            //if url starts with http:// and currentState.url starts with
-            // https://, replace the protocol in url
-            if (url.indexOf("http://", 0) === 0)
-            {
-                var urlNoProt = url.replace(/.*?:\/\//g, "");
-                var curProt = currentState.url.split("/")[0];
-                url = curProt + "//" + urlNoProt;
+            if (showIndex === true) {
+                $latestPost.hide();
+            } else {
+                $latestPost.show();
+                $postIndex.hide();
             }
-            
-            // If the requested url is not the current states url push
-            // the new state and make the ajax call.
-            if (url !== currentState.url.replace(/\/$/, "")) {
-                console.log(url + '!=' + currentState.url.replace(/\/$/, ""))
-                console.log('Changing URL Parameters')
-                loading = true;
 
-                // Check if we need to show the post index after we've
-                // loaded the new content
-                if ($(this).hasClass('js-show-index') || $(this).parent('.pagination').length > 0) {
-                     console.log('has class js-show-index')
-                    showIndex = true;
-                }
+            // Re run fitvid.js
+            $newContent.fitVids();
+
+            $ajaxContainer.html($newContent);
+            $ajaxContainer.fadeIn(500);
+
+            NProgress.done();
+
+            loading = false;
+            showIndex = false;
+            /* Add on's */
+            prettyPrint();
+            updateSocialLinks();
+            initPostStubs();
+            $latestPost = $('.latest-post');
+            $postIndex = $('.post-index');
+        });
+    }).fail(function() {
+        // Request fail
+        NProgress.done();
+        location.reload();
+    });
+});
+
+//Attach handler for ajax requests
+
+$('body').on('click', '.js-ajax-link, .pagination a, .post-tags a, .post-header a', function(e) {
+    ajaxClick( $(this), e );
+});
+
+// Handle hyperlink ajax requests
+function ajaxClick(thisObj, eObj) {
+    eObj.preventDefault();
+    $('#search-field').val('');
+
+    if (loading === false) {
+        var currentState = History.getState();
+        var url = thisObj.attr('href');
+        var title = thisObj.attr('title') || null;
+
+        //if url starts with http:// and currentState.url starts with
+        // https://, replace the protocol in url
+        if (url.indexOf("http://", 0) === 0)
+        {
+            var urlNoProt = url.replace(/.*?:\/\//g, "");
+            var curProt = currentState.url.split("/")[0];
+            url = curProt + "//" + urlNoProt;
+        }
+        
+        // If the requested url is not the current states url push
+        // the new state and make the ajax call.
+        if (url !== currentState.url.replace(/\/$/, "")) {
+            console.log(url + '!=' + currentState.url.replace(/\/$/, ""))
+            console.log('Changing URL Parameters')
+            loading = true;
+
+            // Check if we need to show the post index after we've
+            // loaded the new content
+            if (thisObj.hasClass('js-show-index') || thisObj.parent('.pagination').length > 0) {
+                 console.log('has class js-show-index')
+                showIndex = true;
+            }
+
+            NProgress.start();
+
+            History.pushState({}, title, url);
+        } else {
+            // Swap in the latest post or post index as needed
+
+            if (searchResultContainer == "visible") {
+                searchResultContainer = "hidden"
+                $('.container-search').fadeOut(300, function() {
+                    $('.container-post').fadeIn(300);
+                });
+            }
+
+            if (thisObj.hasClass('js-show-index')) {
+                console.log('Same url, link contained js-show-index')
+                console.log($())
+                console.log($latestPost)
+                $('html, body').animate({'scrollTop': 0});
 
                 NProgress.start();
 
-                History.pushState({}, title, url);
+                $latestPost.fadeOut(300, function() {
+                    $postIndex.fadeIn(300);
+                    NProgress.done();
+                });
             } else {
-                // Swap in the latest post or post index as needed
+                console.log('Same url, link missing js-show-index')
+                console.log($())
+                console.log($latestPost)
+                $('html, body').animate({'scrollTop': 0});
 
-                if (searchResultContainer == "visible") {
-                    searchResultContainer = "hidden"
-                    $('.container-search').fadeOut(300, function() {
-                        $('.container-post').fadeIn(300);
-                    });
-                }
+                NProgress.start();
 
-                if ($(this).hasClass('js-show-index')) {
-                    console.log('Same url, link contained js-show-index')
-                    console.log($())
-                    console.log($latestPost)
-                    $('html, body').animate({'scrollTop': 0});
-
-                    NProgress.start();
-
-                    $latestPost.fadeOut(300, function() {
-                        $postIndex.fadeIn(300);
-                        NProgress.done();
-                    });
-                } else {
-                    console.log('Same url, link missing js-show-index')
-                    console.log($())
-                    console.log($latestPost)
-                    $('html, body').animate({'scrollTop': 0});
-
-                    NProgress.start();
-
-                    $postIndex.fadeOut(300, function() {
-                        $latestPost.fadeIn(300);
-                        NProgress.done();
-                    });
-                }
+                $postIndex.fadeOut(300, function() {
+                    $latestPost.fadeIn(300);
+                    NProgress.done();
+                });
             }
         }
-    });
-
+    }
+}
 });
 
+
 /**
- * @fileoverview
- * Registers a language handler for YAML.
- *
+ * Functions needed after each ajax reload
+ */
+
+function updateSocialLinks() {
+  $('#facebook-share-button, #facebook-share-button-tail').attr("href", "https://facebook.com/sharer/sharer.php?u=http%3A%2F%2Fchadsheets.com" + window.location.pathname.replace(/\//g,"%2F"));
+  $('#google-share-button, #google-share-button-tail').attr("href", "https://plus.google.com/share?url=http%3A%2F%2Fchadsheets.com" + window.location.pathname.replace(/\//g,"%2F"));
+  $('#twitter-share-button, #twitter-share-button-tail').attr("href", "https://twitter.com/intent/tweet/?text=" + document.title.replace(/ /g,"%20") + ";&url=http%3A%2F%2Fchadsheets.com" + window.location.pathname.replace(/\//g,"%2F"));
+  $('#linkedin-share-button, #linkedin-share-button-tail').attr("href", "https://www.linkedin.com/shareArticle?mini=true&url=http%3A%2F%2Fchadsheets.com" + window.location.pathname.replace(/\//g,"%2F") + "&title=" + document.title.replace(/ /g,"%20") + "&summary=" + document.title.replace(/ /g,"%20") + "&source=http%3A%2F%2Fchadsheets.com");
+}
+
+function initPostStubs() {
+    $('.post-stub-left').mouseover(function() {
+      $(this).css('cursor','pointer')
+      $(this).attr('href', $(this).find("a:first").attr("href"))
+      $(this).css('border-left', '2px solid #417a97')
+      $(this).click(function(e){ ajaxClick( $(this), e ) })
+    }); $('.post-stub-left').mouseout(function() {
+      $(this).css('border-left', '2px solid #FFFFFF')
+    });
+    $('.post-stub-right').mouseover(function() {
+      $(this).css('cursor','pointer')
+      $(this).attr('href', $(this).find("a:first").attr("href"))
+      $(this).css('border-right', '2px solid #417a97')
+      $(this).click(function(e){ ajaxClick( $(this), e ) })
+    }); $('.post-stub-right').mouseout(function() {
+      $(this).css('border-right', '2px solid #FFFFFF')
+    });
+}
+
+
+/**
+ * GitHub style code box. Registers a language handler for YAML.
  * @author ribrdb
  */
 
